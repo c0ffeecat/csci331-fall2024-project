8aea4960071687b4525b1b0f3e3c87742347ebe3
aidan <aidankeefe486@gmail.com>
2024-11-08 18:11:05 -0700

    more

--- root/server.js
+++ root/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 
+used = true
 
 app.use(express.json());//req.body
 
@@ -9,8 +10,15 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/getInfo", (req,res) => {
-    res.send("changed")
+app.get("/getInfo", (req,res) => {
+    if (used){
+        res.send("changed")
+        used = false
+    }else{
+        res.send("unchanged")
+        used = true
+    }
+    
 })
 
 app.listen(3000, () => {
