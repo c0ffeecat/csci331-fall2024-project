6556b4373f807847ddce7a53622b64971461341c
aidan <aidankeefe486@gmail.com>
2024-11-22 10:15:31 -0700

    made a change to accept files as well

--- root/server.js
+++ root/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const multer = require("multer");
 const app = express()
 const Database = require("better-sqlite3")
 const path = require('path');
@@ -6,10 +7,26 @@ const dbPath = path.resolve(__dirname, '../images.db');
 const db = new Database(dbPath)
 
 
+
+//config for uploading files
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, 'public/images')); // Save files to 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+  });
+
+const upload = multer({storage});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());//req.body
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
 
 const addNewImage = db.prepare("INSERT INTO IMAGES (IMG_URL) VALUES (?)");
 const getAllImages = db.prepare("SELECT * FROM IMAGES");
@@ -19,8 +36,15 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.post("/upload", (req,res) => {
+app.post("/upload", upload.single("file"),(req,res) => {
     let input = req.body.url
+    let image = req.file
+    if (!input && image){
+        input = "images/" + image.filename
+    }else if(!image){
+        res.send("<p>Error uploading<p/>")
+        return
+    }
     addNewImage.run(input);
     var rows = getAllImages.all().reverse();
     var html = "";
